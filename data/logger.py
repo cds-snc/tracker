22195432012ddc9ae079280e9145525caeae6c6c
David Buckley <buckley.w.david@gmail.com>
2018-07-17 11:38:40 -0400

    move handlers to the root logger, add syslog handler

--- data/logger.py
+++ data/logger.py
@@ -1,5 +1,22 @@
 import logging
+from logging.handlers import SysLogHandler
+import os
 
+# Configure the root logger with handlers
+_syslog_handler = SysLogHandler(address=os.environ.get("TRACKER_SYSLOG", "/dev/log"))
+_syslog_handler.setLevel(logging.ERROR)
+
+_stream_handler = logging.StreamHandler()
+_stream_handler.setLevel(logging.INFO)
+
+_formatter = logging.Formatter(
+    fmt="%(asctime)s - %(levelname)s - %(name)s: %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S%z",
+)
+_syslog_handler.setFormatter(_formatter)
+_stream_handler.setFormatter(_formatter)
+logging.getLogger().addHandler(_syslog_handler)
+logging.getLogger().addHandler(_stream_handler)
 
 def unwrap_exception_message(exc: BaseException, join: str = " - ") -> str:
     if exc.__context__:
@@ -12,14 +29,5 @@ def unwrap_exception_message(exc: BaseException, join: str = " - ") -> str:
 def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-
-    if not logger.hasHandlers():
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter(
-            fmt="%(asctime)s - %(levelname)s - %(name)s: %(message)s",
-            datefmt="%Y-%m-%dT%H:%M:%S%z",
-        )
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
 
     return logger
