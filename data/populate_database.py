2662005c7262520bc7850528b8fa47c749976928
Ethan Foss <fossliam@outlook.com>
2019-08-15 16:01:04 -0300

    Altered populate_database insertions

--- data/populate_database.py
+++ data/populate_database.py
@@ -1,12 +1,14 @@
 import click
 import csv
+import os
 from data import models
 
 def populate(ctx: click.core.Context):
     with models.Connection(ctx.obj.get("connection_string")) as connection:
         connection.input_domains.clear()
-        file = open('data/domains.csv', 'r')
-        curReader = csv.reader(file, delimiter=',')
-        for curRow in curReader:
-            row_dict = {0: curRow[0]}
-            connection.domain_input.create(row_dict)
+        domain_path = str(os.getcwd()) + '/csv/domains.csv'
+        with open(domain_path, 'r') as file:
+            curReader = csv.reader(file, delimiter=',')
+            for curRow in curReader:
+                row_dict = {'domain': curRow[0]}
+                connection.domain_input.create(row_dict)
