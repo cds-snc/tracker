995d94c5ef0b083a7b97fb351c99eb99f5c8cc32
Ethan Foss <fossliam@outlook.com>
2019-08-17 13:15:43 -0300

    Altered input database insertions to include owner data

--- data/populate_database.py
+++ data/populate_database.py
@@ -8,14 +8,15 @@ from data import models
 LOGGER = logger.get_logger(__name__)
 
 def populate(ctx: click.core.Context):
+    first_row = True
     with models.Connection(ctx.obj.get("connection_string")) as connection:
         connection.input_domains.clear()
         domain_path = str(os.getcwd()) + '/csv/domains.csv'
         with open(domain_path, 'r') as file:
             curReader = csv.reader(file, delimiter=',')
             for curRow in curReader:
-                row_dict = {'domain': curRow[0]}
-                if connection.domain_input.find(row_dict) is not None:
+                row_dict = {'domain': curRow[0], 'organization_en': curRow[2], 'organization_fr': curRow[3]}
+                if len(list(connection.domain_input.find(row_dict))) is 0 and not first_row:
                     try:
                         connection.domain_input.create(row_dict)
                     except pymongo.errors.DocumentTooLarge:
@@ -37,4 +38,5 @@ def populate(ctx: click.core.Context):
                         LOGGER.exception(
                             "An unknown error was encountered while inserting domains into the input database."
                             " Exception details: %s", str(exc))
+                first_row = False
         connection.close()
