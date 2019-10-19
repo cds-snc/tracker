9f4319cd59935d40fcac3c63c1b97da128beb55f
Ethan Foss <fossliam@outlook.com>
2019-10-19 16:11:08 -0300

    Removed bare exception handlers

--- data/processing.py
+++ data/processing.py
@@ -192,9 +192,6 @@ def run(date: typing.Optional[str], connection_string: str, batch_size: typing.O
         except pymongo.errors.PyMongoError:
             LOGGER.exception("An error was encountered while inserting domains into the database"
                              " (PyMongoError).")
-        except Exception as exc:
-            LOGGER.exception("An unknown error was encountered while inserting domains into the database."
-                             " Exception details: %s", str(exc))
 
         LOGGER.info("Clearing organizations.")
         connection.organizations.clear(batch_size=batch_size)
@@ -219,9 +216,6 @@ def run(date: typing.Optional[str], connection_string: str, batch_size: typing.O
         except pymongo.errors.PyMongoError:
             LOGGER.exception("An error was encountered while inserting organizations into the database"
                              " (PyMongoError).")
-        except Exception as exc:
-            LOGGER.exception("An unknown error was encountered while inserting organizations into the database."
-                             " Exception details: %s", str(exc))
 
         try:
             LOGGER.info("Replacing government-wide totals.")
@@ -241,9 +235,6 @@ def run(date: typing.Optional[str], connection_string: str, batch_size: typing.O
         except pymongo.errors.PyMongoError:
             LOGGER.exception("An error was encountered while replacing government-wide totals within the"
                              " database (PyMongoError).")
-        except Exception as exc:
-            LOGGER.exception("An unknown error was encountered while replacing government-wide totals within the"
-                             " database. Exception details: %s", str(exc))
 
         LOGGER.info("Saving report to historical collection")
         report2 = report.copy()
